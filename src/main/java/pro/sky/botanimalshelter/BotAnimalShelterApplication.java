7a6b611b8a9f3af20c0010b2d2da5e21b336dbba
Smokvin <alex.smokvin@gmail.com>
2023-10-18 17:42:01 +0300

    initial commit

---
+++ src/main/java/pro/sky/botanimalshelter/BotAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.botanimalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotAnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotAnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/botanimalshelter/BotAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.botanimalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
