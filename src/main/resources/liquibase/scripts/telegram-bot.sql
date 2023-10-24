335a8b6169f9d2b760f7f7599b8d81424b406567
Smokvin <alex.smokvin@gmail.com>
2023-10-24 21:18:49 +0300

    Создал таблицы в liquibase для БД. Добавил еще одну зависимость в pom.xml

--- src/main/resources/liquibase/scripts/telegram-bot.sql
+++ src/main/resources/liquibase/scripts/telegram-bot.sql
@@ -1,1 +1,37 @@
 -- liquibase formatted sql
+
+-- changeset asmokvin:1
+CREATE table pet_shelter
+(
+    id                    SERIAL PRIMARY KEY,
+    name                  TEXT,
+    location              TEXT,
+    locatinon_explanation TEXT,
+    specialists_info      TEXT
+);
+
+-- changeset asmokvin:2
+CREATE table users
+(
+    id             BIGSERIAL PRIMARY KEY,
+    id_chat        BIGINT,
+    name           TEXT,
+    phone          TEXT,
+    email          TEXT,
+    location       TEXT,
+    pet_relation   TEXT,
+    loved_specimen TEXT
+);
+
+-- changeset asmokvin:3
+CREATE table pet
+(
+    id              BIGSERIAL PRIMARY KEY,
+    name            TEXT,
+    color           TEXT,
+    bread           TEXT,
+    birth_date      TIMESTAMP,
+    users           TEXT,
+    adoption_status TEXT,
+    pet_shelter     TEXT
+);
