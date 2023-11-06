0e659a2bfd752402a91eae01c9483b3476df628a
Antonov333 <antonovsergei333@yahoo.com>
2023-11-06 10:08:13 +0300

    Pet entity completed, telegram-bot.sql file changed a little to fit actual entities

--- src/main/java/pro/sky/botanimalshelter/model/ShelterMessage.java
+++ src/main/java/pro/sky/botanimalshelter/model/ShelterMessage.java
@@ -1,75 +1,13 @@
 package pro.sky.botanimalshelter.model;
 
-import javax.persistence.*;
 import java.util.Objects;
 
 /**
  * Accordingly to Specification bot is to provide a lot of information to user
- * <br>So, ShelterMessage class is a model of messages described by Spec. ShelterMessage also
- * indicates actions to do if required by Spec on the point
+ * <br>So, ShelterMessage class is a model of messages described by Spec and
+ * indicate actions to do if necessary
  */
-@Entity
-@Table(name = "shelter_book")
 public class ShelterMessage {
-    /**
-     * {@code id} is database identifier
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "shelter_id")
-    private long shelterId;
-
-    /**
-     * stage number accordingly to Spec
-     */
-    @Column(name = "stage")
-    private int stage;
-
-    /**
-     * point order number as it listed in Spec
-     */
-    @Column(name = "point")
-    private int point;
-
-    /**
-     * Unique number to specify procedure to perform if it required by Spec
-     * At the other hand, stage and messageNumber could exactly point of Spec, so
-     * actionId may be redundant.
-     * Dear Team, please tell me what you think about this
-     */
-    @Column(name = "action_id")
-    private long actionId;
-
-    /**
-     * short title, kind of name of message
-     */
-    @Column(name = "title")
-    private String title;
-
-    /**
-     * message to be delivered to user
-     */
-    @Column(name="text_info")
-    private String textInfo;
-
-    public ShelterMessage() {
-    }
-
-    public ShelterMessage(Long id, long shelterId, int stage, int point, long actionId, String title, String textInfo) {
-        this.id = id;
-        this.shelterId = shelterId;
-        this.stage = stage;
-        this.point = point;
-        this.actionId = actionId;
-        this.title = title;
-        this.textInfo = textInfo;
-    }
-
-    public long getShelterId() {
-        return shelterId;
-    }
 
     public Long getId() {
         return id;
@@ -77,6 +15,58 @@ public class ShelterMessage {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    /**
+     * {@code id} is database identifier
+     */
+    private Long id;
+
+    private long shelterId;
+
+    /**
+     * stage number accordingly to Spec
+     */
+    private int stage;
+
+    /**
+     * message order number as it listed in Spec
+     */
+    private int messageNumber;
+
+    /**
+     * Unique number to specify procedure to perform if it required by Spec
+     * At the other hand, stage and messageNumber could exactly point of Spec, so
+     * actionId may be redundant.
+     * Dear Team, please tell me what you think about this
+     */
+    private long actionId;
+
+    /**
+     * short title, kind of name of message
+     */
+    private String title;
+
+    /**
+     * message to be delivered to user
+     */
+    private String text;
+
+    public ShelterMessage() {
+    }
+
+    public ShelterMessage(Long id, long shelterId, int stage, int messageNumber, long actionId, String title, String text) {
+        this.id = id;
+        this.shelterId = shelterId;
+        this.stage = stage;
+        this.messageNumber = messageNumber;
+        this.actionId = actionId;
+        this.title = title;
+        this.text = text;
+    }
+
+    public long getShelterId() {
+        return shelterId;
     }
 
     public void setShelterId(long shelterId) {
@@ -91,12 +81,12 @@ public class ShelterMessage {
         this.stage = stage;
     }
 
-    public int getPoint() {
-        return point;
+    public int getMessageNumber() {
+        return messageNumber;
     }
 
-    public void setPoint(int point) {
-        this.point = point;
+    public void setMessageNumber(int messageNumber) {
+        this.messageNumber = messageNumber;
     }
 
     public long getActionId() {
@@ -115,12 +105,12 @@ public class ShelterMessage {
         this.title = title;
     }
 
-    public String getTextInfo() {
-        return textInfo;
+    public String getText() {
+        return text;
     }
 
-    public void setTextInfo(String textInfo) {
-        this.textInfo = textInfo;
+    public void setText(String text) {
+        this.text = text;
     }
 
     @Override
@@ -128,10 +118,10 @@ public class ShelterMessage {
         return "ShelterMessage{" +
                 "shelterId=" + shelterId +
                 ", stage=" + stage +
-                ", messageNumber=" + point +
+                ", messageNumber=" + messageNumber +
                 ", actionId=" + actionId +
                 ", title='" + title + '\'' +
-                ", text='" + textInfo + '\'' +
+                ", text='" + text + '\'' +
                 '}';
     }
 
@@ -139,11 +129,11 @@ public class ShelterMessage {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof ShelterMessage that)) return false;
-        return Objects.equals(getId(), that.getId());
+        return getShelterId() == that.getShelterId() && getStage() == that.getStage() && getMessageNumber() == that.getMessageNumber() && getActionId() == that.getActionId() && Objects.equals(getTitle(), that.getTitle()) && Objects.equals(getText(), that.getText());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId());
+        return Objects.hash(getShelterId(), getStage(), getMessageNumber(), getActionId(), getTitle(), getText());
     }
 }
