29e83977b1fa1e16b0c698737df7af3681faadb7
Antonov333 <antonovsergei333@yahoo.com>
2023-11-08 16:49:46 +0300

    BotLogRecord model is composed, equals() and hash() methods updated in ShelterMessage model. JPA Annotations and SQL scripts are to be composed next

---
+++ src/main/java/pro/sky/botanimalshelter/model/ActionCondition.java
@@ -0,0 +1,7 @@
+package pro.sky.botanimalshelter.model;
+
+public enum ActionCondition {
+    TO_DO,
+    IN_PROGRESS,
+    DONE
+}
---
+++ src/main/java/pro/sky/botanimalshelter/model/Conclusion.java
@@ -0,0 +1,8 @@
+package pro.sky.botanimalshelter.model;
+
+public enum Conclusion {
+    DONE,
+    FAILED,
+    PET_IS_GIVEN_ON_TRIAL_ADOPTION,
+    ADOPTION_APPROVED
+}
