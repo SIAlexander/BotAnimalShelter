7dea211d53d67b558f8b1ca92e074dc3bdaa2e52
Antonov333 <antonovsergei333@yahoo.com>
2023-11-14 14:04:21 +0300

    Созданы эндпойнты для сохранения, удаления сущностей кинологов (класс Handler) и просмотра списка кинологов

--- src/main/java/pro/sky/botanimalshelter/volunteercrud/HandlerController.java
+++ src/main/java/pro/sky/botanimalshelter/volunteercrud/HandlerController.java
@@ -1,9 +1,13 @@
 package pro.sky.botanimalshelter.volunteercrud;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.botanimalshelter.model.Handler;
 import pro.sky.botanimalshelter.service.HandlerService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/handler")
@@ -17,5 +21,28 @@ public class HandlerController {
     @GetMapping("")
     public String welcome() {
         return "Добро пожаловать в контроллер кинолога! -- Welcome to Handler Controller!";
+    }
+
+    /**
+     * Сохраняем кинолога -- Save dog trainer
+     */
+    @GetMapping("/save/{name}/{phone}/{shelterId}")
+    public Handler save(@PathVariable("name") String name, @PathVariable("phone") String phone,
+                        @PathVariable() long shelterId) {
+        return handlerService.saveHandler(name, phone, shelterId);
+    }
+
+    @GetMapping("/delete/{id}")
+    public Handler deleteHandler(@PathVariable("id") long id) {
+        return handlerService.deleteHandlerById(id);
+    }
+
+
+    /**
+     * Просмотр списка кинологов -- View dog trainer list
+     */
+    @GetMapping("/view")
+    public List<Handler> viewHandlers() {
+        return handlerService.findAllHandlers();
     }
 }
