b92ceca519e8bb097b2837ba4465e3ebba1dc583
Antonov333 <antonovsergei333@yahoo.com>
2023-11-15 23:02:02 +0300

    Делаю тесты...

--- src/test/java/pro/sky/botanimalshelter/HandlerServiceTest.java
+++ src/test/java/pro/sky/botanimalshelter/HandlerServiceTest.java
@@ -1,9 +1,15 @@
 package pro.sky.botanimalshelter;
 
+import liquibase.pro.packaged.H;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +19,11 @@ import pro.sky.botanimalshelter.model.PetShelter;
 import pro.sky.botanimalshelter.repository.HandlerRepository;
 import pro.sky.botanimalshelter.service.HandlerService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 
 @SpringBootTest
@@ -32,29 +38,50 @@ public class HandlerServiceTest {
     @InjectMocks
     private HandlerService handlerServiceMocked;
 
+    @BeforeEach
+    public void setup() {
+        openMocks(this);
+    }
+
+    private static PetShelter getRightPetShelter() {
+        final PetShelter rightPetShelter = new PetShelter();
+        rightPetShelter.setName("Right Pet Shelter");
+        rightPetShelter.setId(1L);
+        return rightPetShelter;
+    }
+
+    private static Handler getHandler(long id) {
+        long l = id * 1111111;
+        String phone = String.valueOf(l);
+        String name = "Name" + id;
+        Handler handler = new Handler(name, phone, getRightPetShelter());
+        handler.setId(id);
+        return handler;
+    }
+
     @Test
     public void saveHandlerEntityTest() {
+        Handler handler = getHandler(1);
         assertThrows(RuntimeException.class, () -> handlerServiceMocked.saveHandler(null));
+        when(handlerRepositoryMock.save(handler)).thenReturn(handler);
+        assertEquals(handler, handlerRepositoryMock.save(handler));
+        assertEquals(handler, handlerServiceMocked.saveHandler(handler));
     }
 
     @Test
     public void saveHandlerArgsTest() {
 
-        PetShelter rightPetShelter = new PetShelter();
-
-        rightPetShelter.setName("Right Pet Shelter");
-        rightPetShelter.setId(1L);
+        PetShelter rightPetShelter = getRightPetShelter();
 
         final Handler rightHandler = new Handler("Right", "111-111-1111", rightPetShelter);
         rightHandler.setId(1L);
+
+        logger.info(rightHandler.toString());
 
         assertThrows(RuntimeException.class,
                 () -> handlerServiceMocked.saveHandler("", "112", 1));
 
         logger.info(handlerServiceMocked.toString());
-
-//        when(handlerRepositoryMock.save(any(Handler.class))).thenReturn(rightHandler);
-//        assertEquals(rightHandler, handlerServiceMocked.saveHandler(rightHandler));
 
     }
 
